ba7a2b36b63da96ab4bb9b0dea990763dbc2042f
Moiz Ahmed <ahmedmoiz201@gmail.com>
2024-03-19 01:05:03 +0500

    Update files i.e. Adding API using POST

--- Ajax_RESTFUL_API/api_jq.js
+++ Ajax_RESTFUL_API/api_jq.js
@@ -1,47 +1,60 @@
-// $(document).ready(function() {
-//     // Define the API endpoint
-//     var apiLocation = "https://jsonplaceholder.typicode.com/posts";
+$(document).ready(function() {
+    var apiLocation = "https://jsonplaceholder.typicode.com/posts";
 
-//     // Make AJAX request to fetch data from the API
-//     $.ajax({
-//         url: apiLocation,
-//         method: "GET",
-//         dataType: "json",
-//         success: function (data) {
-//             var productsList = $("#productsList");
-//             productsList.empty();
-//             $.each(data, function (index, product) {
-//                 productsList.append(
-//                   `<div class="mb-3">
-//                         <h3>${product.title}</h3>
-//                         <div>${product.content}</div>
-//                         <div>
-//                             <button class="btn btn-warning mt-3" data-id="${product.id}">Edit</button>
-//                             <button class="btn btn-danger btn-sm mr-2 btn-del" data-id="${product.id}">Delete</button>
-//                         </div>
-//                     </div>
-//                     <hr />
-//                     `
-//                 );
-//               });
-//             },
-//             error: function (error) {
-//               console.error("Error fetching stories:", error);
-//             },
-//           });
-//         })
+    function fetchProducts(){
+        $.ajax({
+            url: apiLocation,
+            method: "GET",
+            dataType: "json",
+            success: function (data) {
+                var productsList = $("#productsList");
+                productsList.empty();
+                $.each(data, function (index, product) {
+                    productsList.append(
+                    `<div class="mb-3">
+                            <h3>${product.title}</h3>
+                            <h3>${product.cat}</h3>
+                            <div>${product.content}</div>
+                            <div>
+                                <button class="btn btn-warning mt-3" data-id="${product.id}">Edit</button>
+                                <button class="btn btn-danger mt-3" data-id="${product.id}">Delete</button>
+                            </div>
+                        </div>
+                        <hr />
+                        `
+                    );
+                });
+                },
+                error: function (error) {
+                console.error("Error fetching products:", error);
+                },
+            });
+            }        
 
-//         function deleteProduct() {
-//             let productId = $(this).attr("data-id");
-//             $.ajax({
-//               url: "https://jsonplaceholder.typicode.com/posts" + productId,
-//               method: "DELETE",
-//               success: function () {
-//                 displayStories(); // Refresh the list after deleting a story
-//               },
-//               error: function (error) {
-//                 console.error("Error deleting story:", error);
-//               },
-//             });
-//           }
+    fetchProducts(); 
 
+    $("#createBtn").click(function(event) {
+        event.preventDefault();
+        var name = $("#createTitle").val();
+        var category = $("#createCategory").val();
+        var detail = $("#createContent").val();
+
+        $.ajax({
+            url: apiLocation,
+            method: "POST",
+            data: {
+                title: name,
+                cat: category,
+                content: detail,
+                userId: 1 
+            },
+            success: function(response) {
+                console.log("Product created:", response);
+                fetchProducts();
+            },
+            error: function(xhr, status, error) {
+                console.error("Error creating product:", error);
+            }
+        });
+    });
+})
