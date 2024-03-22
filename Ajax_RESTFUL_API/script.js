79b5872378495fc7a5d7437dd37a4d1f92fa3573
Moiz Ahmed <ahmedmoiz201@gmail.com>
2024-03-23 00:35:09 +0500

    Updated files i.e. Contact us form validation

--- Ajax_RESTFUL_API/script.js
+++ Ajax_RESTFUL_API/script.js
@@ -9,53 +9,53 @@ document.addEventListener("DOMContentLoaded", function () {
       form.reset();
   });
 
-  // function validateForm() {
-  //   let valid = true;
-  //   resetErrors();
+  function validateForm() {
+    let valid = true;
+    resetErrors();
 
-  //   const firstName = document.getElementById("firstName").value;
-  // //   const lastName = document.getElementById("lastName").value;
-  // //   const email = document.getElementById("email").value;
-  // //   const message = document.getElementById("message").value;
+    const firstName = document.getElementById("firstName").value;
+  //   const lastName = document.getElementById("lastName").value;
+  //   const email = document.getElementById("email").value;
+  //   const message = document.getElementById("message").value;
 
-  //   const firstNameError = document.getElementById("firstNameError");
-  // //   const lastNameError = document.getElementById("lastNameError");
-  // //   const emailError = document.getElementById("emailError");
-  // //   const messageError = document.getElementById("messageError");
+    const firstNameError = document.getElementById("firstNameError");
+  //   const lastNameError = document.getElementById("lastNameError");
+  //   const emailError = document.getElementById("emailError");
+  //   const messageError = document.getElementById("messageError");
 
-  //   if (firstName.trim() === "") {
-  //     displayError(firstNameError, "Please enter your First Name");
-  //     valid = false;
-  //   }
+    if (firstName.trim() === "") {
+      displayError(firstNameError, "Please enter your First Name");
+      valid = false;
+    }
 
-  //   if (lastName.trim() === "") {
-  //     displayError(lastNameError, "Please enter your Last Name");
-  //     valid = false;
-  //   }
+    if (lastName.trim() === "") {
+      displayError(lastNameError, "Please enter your Last Name");
+      valid = false;
+    }
 
-  //   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  //   if (!emailRegex.test(email)) {
-  //     displayError(emailError, "Invalid email address");
-  //     valid = false;
-  //   }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      displayError(emailError, "Invalid email address");
+      valid = false;
+    }
 
-  //   if (message.trim() === "") {
-  //     displayError(messageError, "Please enter your message");
-  //     valid = false;
-  //   }
+    if (message.trim() === "") {
+      displayError(messageError, "Please enter your message");
+      valid = false;
+    }
 
-  //   return valid;
-  // }
+    return valid;
+  }
 
-  // function displayError(errorElement, errorMessage) {
-  //   errorElement.textContent = errorMessage;
-  // }
+  function displayError(errorElement, errorMessage) {
+    errorElement.textContent = errorMessage;
+  }
 
-  // function resetErrors() {
-  //   const errorElements = document.querySelectorAll(".error-message");
-  //   errorElements.forEach(function (errorElement) {
-  //     errorElement.textContent = "";
-  //   });
-  // }
+  function resetErrors() {
+    const errorElements = document.querySelectorAll(".error-message");
+    errorElements.forEach(function (errorElement) {
+      errorElement.textContent = "";
+    });
+  }
 
 });
