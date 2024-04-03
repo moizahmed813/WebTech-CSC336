0cdc9dd3f1b7bf61129a6144451e8333e4dc3b52
Moiz Ahmed <ahmedmoiz201@gmail.com>
2024-04-03 13:00:23 +0500

    Initial Commit i.e. HTML design for Al-Jazeera with CSS and Script.JS for event handling

---
+++ FA21-BCS-030_LabMidExam/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var images = document.querySelectorAll('.grid-item img');
+    images.forEach(function(image) {
+        image.addEventListener('mouseover', function() {
+            var imageName = this.getAttribute('alt');
+            document.getElementById('main-menu').getElementsByTagName('li')[0].innerText = imageName;
+        });
+        
+        image.addEventListener('mouseout', function() {
+            document.getElementById('main-menu').getElementsByTagName('li')[0].innerText = 'Main Menu Item';
+        });
+    });
+});
