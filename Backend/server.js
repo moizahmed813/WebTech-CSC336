469019f8837cb8d0b1774742e31003089e6733b6
Moiz Ahmed <ahmedmoiz201@gmail.com>
2024-05-18 02:07:33 +0500

    Updated files

--- Backend/server.js
+++ Backend/server.js
@@ -8,11 +8,6 @@ const dotenv = require("dotenv").config();
 const bodyParser = require('body-parser');
 
 connectDb();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static('public'));
-
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/Frontend'));
 
